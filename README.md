1bdcc5d3eb0b6f1976aedb60f192ebfae7a78939
Kor de Jong <k.dejong@geo.uu.nl>
2012-10-03 13:39:09 +0200

    WIP making initial version work on Linux

--- README.md
+++ README.md
@@ -11,10 +11,14 @@ Requirements:
 * After installing the `greeter` project, it must be possible to run the executable without changing the environment settings.
 * After moving the install location of the `greeter` project, it must be possible to run the executable without changing the environment settings.
 
+Notes:
+* Targets often depend on shared libraries not build by the installed project, like boost, icu, qt, etc. If we can assume these will exist at the same location on the install machine, we can set `CMAKE_INSTALL_RPATH_USE_LINK_PATH` to `TRUE`. Otherwise we may need to ship these libraries ourselves, and set `CMAKE_INSTALL_RPATH_USE_LINK_PATH` to `FALSE`.
+* Don't confuse installing a project with packaging a project. During installation, you normally don't install targets of other projects. They should already be installed. During packaging, you may want to put all kinds of stuff in the package, including 3rd party shared libraries and shared libraries from several of your own projects.
+* `FIXUP_BUNDLE` can be used to copy prerequisites of exes and dlls into the install area to create a self-contained 'bundle'. This can be used to create self-contained packages.
+
 See also:
 * http://www.vtk.org/Wiki/CMake/Tutorials/Exporting_and_Importing_Targets
 * CMake RPATH wiki page.
 * HDF5 installs manifest file on Windows. We may need that too.
+* http://www.cmake.org/Wiki/BundleUtilitiesExample
 
-Notes:
-* Targets may depend on shared libraries not build by projects, like boost, icu, qt, etc. If we can assume these will exist at the same location on the install machine, we can set `CMAKE_INSTALL_RPATH_USE_LINK_PATH` to `TRUE`. Otherwise we may need to ship these libraries ourselves, and set `CMAKE_INSTALL_RPATH_USE_LINK_PATH` to `FALSE`.
--- environment/scripts/build_hello_cmake.sh
+++ environment/scripts/build_hello_cmake.sh
@@ -15,7 +15,7 @@ rm -fr $world_build $world_install
 mkdir $world_build
 cd $world_build
 
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_INSTALL_PREFIX=$world_install $world_source
+cmake -G "Unix Makefiles" -DCMAKE_MODULE_PATH=$HELLO_CMAKE/environment/templates/cmake -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_INSTALL_PREFIX=$world_install $world_source
 cmake --build . --target install
 
 # Build and install the greeter project. ---------------------------------------
@@ -27,31 +27,68 @@ cd $build_root
 rm -fr $greeter_build $greeter_install
 mkdir $greeter_build
 cd $greeter_build
-
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$build_type -DWORLD_PREFIX=$world_install -DCMAKE_INSTALL_PREFIX=$greeter_install $greeter_source
+cmake -G "Unix Makefiles" -DCMAKE_MODULE_PATH=$HELLO_CMAKE/environment/templates/cmake -DCMAKE_BUILD_TYPE=$build_type -DWORLD_PREFIX=$world_install -DCMAKE_INSTALL_PREFIX=$greeter_install $greeter_source
 # make VERBOSE=1
-cmake --build . --target install
+cd ..
+
+cmake --build $greeter_build
+cmake --build $greeter_build --target install
+cmake --build $greeter_build --target package
 
 # Run executable. --------------------------------------------------------------
+if [ `uname -o 2>/dev/null` ]; then
+    os=`uname -o`
+else
+    os=`uname`
+fi
+
+check_dependencies() {
+    target=$1
+    if [ $os == "GNU/Linux" ]; then
+        chrpath --list $target
+        ldd $target
+    elif [ $os == "Darwin" ]; then
+        otool -L $target
+    elif [ $os == "Cygwin" ]; then
+        cygcheck $target
+    else
+        echo "Unknown OS"
+        exit 1
+    fi
+}
 
 # From build location. Targets are allowed to depend on world's install
 # location.
 $greeter_build/sources/greeter/hcgreeter-static
+check_dependencies $greeter_build/sources/greeter/hcgreeter-shared
 $greeter_build/sources/greeter/hcgreeter-shared
+
+# We cannot run the executable from the install location since it depends on
+# the world shared library which is installed somewhere else.
+
+# From install location.
+$greeter_install/bin/hcgreeter-static
+check_dependencies $greeter_install/bin/hcgreeter-shared
+$greeter_install/bin/hcgreeter-shared
 
 # Move world's install location to make sure its targets are not used by
 # greeter's targets.
+rm -fr ${world_install}_moved
 mv $world_install ${world_install}_moved
 
 # From install location. Targets are not allowed to depend on world's install
 # location.
 $greeter_install/bin/hcgreeter-static
+check_dependencies $greeter_install/bin/hcgreeter-shared
 $greeter_install/bin/hcgreeter-shared
 
 # Move greeter's install location to make sure its targets don't depend on the
 # install location.
+rm -fr ${greeter_install}_moved
 mv $greeter_install ${greeter_install}_moved
 ${greeter_install}_moved/bin/hcgreeter-static
+check_dependencies ${greeter_install}_moved/bin/hcgreeter-shared
 ${greeter_install}_moved/bin/hcgreeter-shared
 
-find $greeter_install
+find ${greeter_install}_moved
+
